b73a1fa2d3623d5aa54948de160546ea4020c690
unknown <kgandhi007380@gmail.com>
2020-04-13 20:35:57 +0530

    changed format

--- src/app/print/print.service.ts
+++ src/app/print/print.service.ts
@@ -26,23 +26,27 @@ export class PrintService {
     this.printEdited.next([...this.prints]);
   }
   printFiles() {
-    const FileData = new FormData();
-
+    let arr: any[] = [];
+    let FileData = new FormData();
     for (let i = 0; i < this.prints.length; i++) {
-      FileData.append("name", this.prints[i].name);
-      FileData.append("numOfPrint", this.prints[i].numOfPrint.toString());
-      FileData.append("pageType", this.prints[i].pageType);
-      FileData.append("size", this.prints[i].size.toString());
-      FileData.append("startPage", this.prints[i].startPage.toString());
-      FileData.append("endPage", this.prints[i].endPage.toString());
-      FileData.append("totalPages", this.prints[i].totalPages.toString());
-      FileData.append("isColor", this.prints[i].isColor.toString());
-      FileData.append("isSingleSided", this.prints[i].isSingleSided.toString());
-      FileData.append("content", this.prints[i].content, this.prints[i].name);
+      // FileData.append("name", this.prints[i].name);
+      // FileData.append("numOfPrint", this.prints[i].numOfPrint.toString());
+      // FileData.append("pageType", this.prints[i].pageType);
+      // FileData.append("size", this.prints[i].size.toString());
+      // FileData.append("startPage", this.prints[i].startPage.toString());
+      // FileData.append("endPage", this.prints[i].endPage.toString());
+      // FileData.append("totalPages", this.prints[i].totalPages.toString());
+      // FileData.append("isColor", this.prints[i].isColor.toString());
+      // FileData.append("isSingleSided", this.prints[i].isSingleSided.toString());
+      // FileData.append("content", this.prints[i].content, this.prints[i].name);
+      arr.push(this.prints[i]);
     }
-    console.log(FileData.getAll("name"));
+    console.log(arr);
+    FileData.append("assets", JSON.stringify(arr));
+    //console.log(FileData.getAll("name"));
+    console.log(JSON.stringify(arr));
     this.httpClient
-      .post("http://54.83.119.157/api/uploadfile", FileData)
+      .post("http://54.83.119.157/api/uploadfile/", FileData)
       .subscribe();
   }
 }
